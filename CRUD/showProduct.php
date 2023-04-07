32cca545c1dc2d799560d82e5a913a6f58b303f6
dinhngocson0708 <116275333+dinhngocson0708@users.noreply.github.com>
2023-04-08 06:22:00 +0700

    Update ShowProduct.php

--- CRUD/showProduct.php
+++ CRUD/showProduct.php
@@ -4,34 +4,45 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="public/img/css/showSp.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <title>Document</title>
+    <style>
+        img{
+            width: 300px;
+            height:300px
+        }
+    </style>
 </head>
 <body>
-    <?php 
+
+        <?php 
         include "connect.php";
         $sql="SELECT * from sanphamadmin";
-        if($kq=$mysqli->query($sql)){
-        while($row=$kq->fetch_array()){
-            echo '<div class="product_list">';
-                echo '<div class="card" style="width: 18rem;">';
-                    echo '<img class="card-img-top" src="' . $row['images'] . '">';
-                    echo '<div class="card-body">';
-                        echo '<h5 class="card-title">' . $row['namesapham'] . '</h5>';
-                        echo '<p class="card-text">' . $row['motasanpham'] . '</p>';
-                        echo '<p class="card-text"><small class="text-muted">' . $row['giasanpham'] . '</small></p>';
-                    echo '</div>';
-                echo '</div>';
+        $result=$mysqli->query($sql);
+        if (mysqli_num_rows($result) > 0) {
+            // Hiển thị sản phẩm
+            echo '<div class="row">';
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<div class="col-md-3">';
+                echo '<div class="card">';
+                echo '<img src="' . $row['images'] . '" class="card-img-top">';
+                echo '<div class="card-body">';
+                echo '<h5 class="card-title">' . $row['namesapham'] . '</h5>';
+                echo '<p class="card-text">' . $row['motasanpham'] . '</p>';
+                echo '<p class="card-text">' . $row['giasanpham'] . '</p>';
+                echo '<a href="xulythemgiohang" class="btn btn-primary">Thêm vào giỏ hàng</a>';
+                echo '</div></div></div>';
+            }
             echo '</div>';
-            
-    
+        } else {
+            echo 'Không có sản phẩm nào';
         }
-    
-        }
+        
+        // Đóng kết nối
+        mysqli_close($mysqli);
+        
     ?>
+    
+    
 </body>
 </html>
